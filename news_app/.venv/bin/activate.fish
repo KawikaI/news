b12f397d7f742f9130e6b05b3167c6dd8a603a40
Kawika Ilac <kawikailac22@gmail.com>
2024-10-22 12:21:51 -0600

    chapter 11 complete

--- news_app/.venv/bin/activate.fish
+++ news_app/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kawikai/Desktop/news/.venv"
+set -gx VIRTUAL_ENV "/Users/kawikai/Desktop/news/news_app/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
